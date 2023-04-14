345ad7ddc9e2936ec74b18b65e904b9525f43b01
mootaz2000 <mootazbendhafer@gmail.com>
2023-04-14 15:15:06 +0100

    first commit

---
+++ first-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './compoments/formulaires';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>this is our first react app</h1> 
+      <h2>new title</h2>
+      <BasicExample/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
